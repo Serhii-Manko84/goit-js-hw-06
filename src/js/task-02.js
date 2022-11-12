c98510c4882da595b835fb2535bbf7239d74f388
Serhii Manko <msa01051984@gmail.com>
2022-11-12 20:34:02 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,12 @@
+"use strict";
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+// Створить окремий елемент <li>. 
+// Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +15,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const nameIngredients = document.querySelector('#ingredients')
+console.log(nameIngredients)
+
+const ingrArray = []
+
+for (let i = 0; i < ingredients.length; i++) {
+  const ingredient = document.createElement('li')
+  ingredient.className = "item"
+  ingredient.textContent = ingredients[i]
+  ingrArray.push(ingredient)
+  
+    // console.log(ingredient)
+}
+
+nameIngredients.append(...ingrArray);
