f0661e2e07e032891b4ce774bee331e19bae99e3
Serhii Manko <msa01051984@gmail.com>
2022-11-15 00:15:09 +0200

    task-9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,55 @@
+"use strict";
 
+// Напиши скрипт управління формою логіна.
+
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно 
+// до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, 
+// що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення 
+// полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - 
+// значенням властивості. 
+// Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів 
+// форми методом reset.
+
+const refs = {
+    elementForm: document.querySelector('.login-form'),
+    elementEmail: document.querySelector('[type="email"]'),
+    elementPassword: document.querySelector('[type="password"]'),
+    elementSubmit: document.querySelector('[type="submit"]'),
+}
+
+const formSubmit = event =>{
+    event.preventDefault()
+    
+    const formsElement = event.currentTarget.elements;
+    const nameEmail = formsElement.email.value;
+    const namePassword = formsElement.password.value
+    
+    
+    if(nameEmail === "" || namePassword === "") {
+        alert ('All fields must be filled')
+    }
+    else {
+        const subEmailPassword = {nameEmail, namePassword}
+        
+        console.log(subEmailPassword)
+        event.currentTarget.reset()
+    }
+}
+
+refs.elementForm.addEventListener('submit', formSubmit)
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,20 @@
+"use strict";
+
+// Напиши скрипт, який змінює кольори фону елемента <body> через 
+// інлайн-стиль по кліку на 
+// button.change-color і виводить значення кольору в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const chancgeColor = document.querySelector('.change-color');
+const bodyColor = document.body;
+
+
+chancgeColor.addEventListener('click', event => {
+event.preventDefault()
+bodyColor.style.backgroundColor = getRandomHexColor();
+
+})
